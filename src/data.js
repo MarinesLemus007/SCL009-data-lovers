a69d4ba0def31b17cd00c1c95283ef1f36dcf190
MarinesLemus007 <marineslemus@gmail.com>
2019-05-02 08:49:50 -0400

    orden por número de pokémon, cambio de logo, se quitaron algunos comentarios

--- src/data.js
+++ src/data.js
@@ -8,31 +8,30 @@
 };
 
 window.example = example;*/
+
+//Función de filtrado por tipo de pokémon
 const filterData = (data, condition) =>{
- const Poketype = data.filter(view => {
-  return (view.type.includes(condition));
+const Poketype = data.filter(view => {
+return (view.type.includes(condition));
 }); 
 
 return Poketype;
 }
-
 window.filterData = filterData;
 
-
-/*const orderByNameAsc = POKEMON.pokemon.map(poke => poke); //console.log(orderByNameAsc);
-orderByNameAsc.sort((original, order) => {
-    return original.name.localeCompare(order.name);
-}); //console.log(orderByNameAsc);
-
-const orderByNameDes = POKEMON.pokemon.map(poke => poke); //console.log(orderByNameAsc);
-orderByNameDes.sort((original, order) => {
-    return order.name.localeCompare(original.name);
-}); //console.log(orderByNameDes);*/
-
-
-/*const OrderNum = (data,decreasing) =>{
-  const PokeOrder = data.sort((a,b)=>b-a){
-    return ()
+const orderData = (data) =>{
+  const Pokeorder = data.reverse(viewTwo => {
+    return (viewTwo.data)
+  });
+  
+  return Pokeorder;
   }
-  ;
-}*/
+  window.orderData = orderData;
+
+const orderDataTwo = (data) =>{
+  const PokeOrderTwo = data.sort((a,b) => {
+    return (a.id - b.id)
+  });
+    return PokeOrderTwo;
+  }
+  window.orderDataTwo = orderDataTwo;
--- src/main.js
+++ src/main.js
@@ -1,105 +1,101 @@
 /* Manejo del DOM */
 
+//Costante que encierra el array de los pokemones
 
 const data = window.POKEMON.pokemon;
 
-//console.log(POKEMON.pokemon);
-
-/*const filtro = data.filter( element => {
-    //console.log(element.name);
-});/*
-
-/*console.log(Object.values(POKEMON)); sirve*/
-
-/*const PokeMon = Object.values(POKEMON);
-console.log(POKEMON);
-
-PokeMon.forEach(element => {
-    console.log(element);
-}); sirve*/
-
-/*let PokeGrass = data.filter(view => {
-    return (view.type.includes("Grass"));
-});
-console.log(PokeGrass);*/
-
-/*const namePoke = data.filter(pokeName =>{
-    return pokeName.name;
-});
-console.log(namePoke);*/
-/*for (let i=0; i< data.length; i++){
-    console.log(data[i].img);
-    console.log(data[i].id);
-    console.log(data[i].name);
-    console.log(data[i].type);
-}; */
-
-//orden creciente
-//arr.sort((a,b)=>a-b);
-
-//orden decreciente
-//arr.sort((a,b)=>b-a);
-
+//Card que muestra todos los pokémones en la pantalla inicial
 for (let i=0; i< data.length; i++){
-    document.getElementById("viewPokemon").innerHTML+=
+document.getElementById("viewPokemon").innerHTML+=
 `
-
-          <div class="col-12 col-sm-4 col-md-3">    
-                <div class="card bg-light mb-3" style="max-width: 18rem;" id= "cardPoke" align= "middle">
-                <img src= "${(data[i].img)}" class= "card-img-top" alt= "${(data[i].name)}" style="max-width: 9rem;">
-                <div class="card-body">
-                <p class="card-name">${(data[i].name)}</p>
-                <p class="card-num"> Number: ${(data[i].id)}</p>
-                <p class="card-type"> Type: ${(data[i].type)}</p>
-                </div>
-                </div>
-            </div>
+<div class="col-12 col-sm-6 col-md-4 col-lg-3">    
+    <div class="card bg-light mb-3" style="max-width: 18rem;" id= "cardPoke" align= "middle">
+    <img src= "${(data[i].img)}" class= "card-img-top" alt= "Card image pokémon" style="max-width: 9rem;">
+    <div class="card-body" style="max-width: 9rem;" >
+    <p class="card-name">${(data[i].name)}</p>
+    <p class="card-num"> Number: ${(data[i].id)}</p>
+    <p class="card-type"> Type: ${(data[i].type)}</p>
+    </div>
+    </div>
+</div>
 `
 }
-
 // Muestra los pokemones filtrados por tipo
-/*const filterPokemon =*/ document.getElementById("filterType").addEventListener("change", ()=> {
+document.getElementById("filterType").addEventListener("change", ()=> {
 
-document.getElementById("typePokemon").innerHTML="";
+document.getElementById("viewPokemon").innerHTML="";
+//document.getElementById("cardPoke").reset();
 
 let condition = document.getElementById("filterType").value;
 
 let dataType = window.filterData(data,condition);
 
-//console.log(dataType);
-
-/*const Order = () => {
-    let select = document.getElementById('filterOrder');
-    let value = select.value;
-    let orderResult;
-    if(value == 'Asc'){
-      Result = data.orderByNameAsc;
-    } else if (value == 'Des'){
-      Result = data.orderByNameDes;
-    } else {
-      Result = data.showPokemon;
-    }
-    createCards(Result);*/
-
-document.getElementById("viewPokemon").style.display ="none";
-
 for (let i=0; i< dataType.length; i++){
-document.getElementById("typePokemon").innerHTML+=
-
+document.getElementById("viewPokemon").innerHTML+=
 `
-<div class="col-12 col-sm-4">    
-      <div class="card bg-light mb-3" style="max-width: 18rem;" align= "middle">
-      <img src= "${(dataType[i].img)}" class= "card-img-top" alt= "${(dataType[i].name)}" style="max-width: 9rem;">
-      <div class="card-body">
+<div class="col-12 col-sm-6 col-md-4 col-lg-3">    
+      <div class="card bg-light mb-3" style="max-width: 18rem;" id= "cardType" align= "middle">
+      <img src= "${(dataType[i].img)}" class= "card-img-top" alt= "Card image pokémon type" style="max-width: 9rem;">
+      <div class="card-body" style="max-width: 9rem;" >
       <p class="card-name">${(dataType[i].name)}</p>
       <p class="card-num"> Number: ${(dataType[i].id)}</p>
       <p class="card-type"> Type: ${(dataType[i].type)}</p>
       </div>
       </div>
-  </div>
-  `
+</div>
+`
 } 
+});
 
-}
+// Muestra los pokemones ordenados por número
+document.getElementById("filterOrder").addEventListener("change",() =>{
 
-  ); 
+let conditionOrder = document.getElementById("filterOrder").value;
+    
+    if(conditionOrder === "decreasing"){
+    
+    document.getElementById("viewPokemon").innerHTML="";    
+    let dataOrder = window.orderData(data, conditionOrder);
+    
+//Card que muestra todos los pokémones por orden decreciente
+for (let i=0; i< dataOrder.length; i++){
+    document.getElementById("viewPokemon").innerHTML+=
+    `
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3">    
+        <div class="card bg-light mb-3" style="max-width: 18rem;" id= "cardPoke" align= "middle">
+        <img src= "${(dataOrder[i].img)}" class= "card-img-top" alt= "Card image pokémon" style="max-width: 9rem;">
+        <div class="card-body" style="max-width: 9rem;" >
+        <p class="card-name">${(dataOrder[i].name)}</p>
+        <p class="card-num"> Number: ${(dataOrder[i].id)}</p>
+        <p class="card-type"> Type: ${(dataOrder[i].type)}</p>
+        </div>
+        </div>
+    </div>
+    `
+    }
+    }
+
+ else if(conditionOrder === "growing"){
+    
+    document.getElementById("viewPokemon").innerHTML="";    
+    let dataOrderTwo = window.orderDataTwo(data, conditionOrder);
+ 
+//Card que muestra todos los pokémones por orden creciente
+for (let i=0; i< dataOrderTwo.length; i++){
+    document.getElementById("viewPokemon").innerHTML+=
+    `
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3">    
+        <div class="card bg-light mb-3" style="max-width: 18rem;" id= "cardPoke" align= "middle">
+        <img src= "${(dataOrderTwo[i].img)}" class= "card-img-top" alt= "Card image pokémon" style="max-width: 9rem;">
+        <div class="card-body" style="max-width: 9rem;" >
+        <p class="card-name">${(dataOrderTwo[i].name)}</p>
+        <p class="card-num"> Number: ${(dataOrderTwo[i].id)}</p>
+        <p class="card-type"> Type: ${(dataOrderTwo[i].type)}</p>
+        </div>
+        </div>
+    </div>
+    `
+    }
+    }
+        
+});
